ecbc4af14bf67c53a8954d13b07856c2441dbd3a
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2021-05-16 14:15:37 -0300

    adding usage activity

---
+++ app/src/main/java/br/com/empresa/mobileproject/UsageActivity.kt
@@ -0,0 +1,11 @@
+package br.com.empresa.mobileproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UsageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_usage)
+    }
+}
