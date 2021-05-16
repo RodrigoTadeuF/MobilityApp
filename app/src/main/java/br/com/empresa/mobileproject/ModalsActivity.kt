2214e66c8d840e633b032dbb2224b98ff5998835
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2021-05-16 14:13:10 -0300

    adding skeleton of the project

---
+++ app/src/main/java/br/com/empresa/mobileproject/ModalsActivity.kt
@@ -0,0 +1,11 @@
+package br.com.empresa.mobileproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModalsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modals)
+    }
+}
---
+++ app/src/main/java/br/com/empresa/mobileproject/SavingsActivity.kt
@@ -0,0 +1,11 @@
+package br.com.empresa.mobileproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SavingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_savings)
+    }
+}
