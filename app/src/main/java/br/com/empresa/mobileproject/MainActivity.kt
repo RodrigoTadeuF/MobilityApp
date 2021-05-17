343d64c7820d7ffeacc31f709167ea9b1501bf9a
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2021-05-17 20:38:27 -0300

    adjusting some layouts

--- app/src/main/java/br/com/empresa/mobileproject/MainActivity.kt
+++ app/src/main/java/br/com/empresa/mobileproject/MainActivity.kt
@@ -1,11 +1,37 @@
 package br.com.empresa.mobileproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnModals:Button = findViewById(R.id.modalsButton)
+        var btnUsage:Button = findViewById(R.id.usageButton)
+        var btnSavings:Button = findViewById(R.id.savingsButton)
+
+        btnModals.setOnClickListener(View.OnClickListener {
+            var intent= Intent(this,ModalsActivity::class.java)
+            startActivity(intent)
+        })
+
+        btnUsage.setOnClickListener(View.OnClickListener {
+            var intent= Intent(this,UsageActivity::class.java)
+            startActivity(intent)
+        })
+
+        btnSavings.setOnClickListener(View.OnClickListener {
+            var intent= Intent(this,SavingsActivity::class.java)
+            startActivity(intent)
+        })
+
+
+
+
     }
 }
